0844f25eb5b6549a02669863cc6d81f276cc4201
Pieter-Paulus Vertongen <pieter@aaltra.eu>
2019-08-22 00:52:24 +0200

    prepared and released version 0.1.2

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function createWindow () {
   mainWindow.loadFile('view/index.html')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
